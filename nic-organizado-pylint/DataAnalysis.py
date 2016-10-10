4368a882bda0b2bcecd0f00c89d28bceba224aa8
Nicoli Araujo <npa.matsu@gmail.com>
2016-10-09 20:31:53 -0400

    Threshold e detecção de padrões de bordas ajustado. DataAnalysis recebido do eman

--- nic-organizado-pylint/DataAnalysis.py
+++ nic-organizado-pylint/DataAnalysis.py
@@ -4,14 +4,60 @@ Created on Oct 2, 2016
 @author: Nicoli
 '''
 
-class MyClass(object):
+import pandas as pd
+
+import numpy as np
+
+import seaborn as sns
+
+    
+class Estatistica(object):
     '''
     classdocs
     '''
-
-
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+     
+    def media(self):
+         
+        self.media=self.dataframe.mean()
+    
+    
+    def variancia(self):
+    
+        self.variancia=self.dataframe.var()  
+       
+       
+    
+    def desvio(self):
+        self.desvio=self.dataframe.std()
+    
+    def soma(self):
+        self.soma=self.dataframe.sum()
+    
+    def grafico1(self):
+        sns.barplot(x=self.media[0],y=self.media[1],data=self.media)
+        sns.savefig("poe o caminho aqui")
+    
+    def grafico2(self):
+        sns.barplot(x=self.variancia[0],y=self.variancia[1],data=self.variancia)
+        sns.savefig()
+    
+    def grafico3(self):
+        sns.barplot(x=self.desvio[0],y=self.desvio[1],data=self.desvio)
+        sns.savefig()
+    
+    def grafico4(self):
+        sns.barplot(x=self.soma[0],y=self.desvio[1],data=self.soma)
+        sns.savefig()
+    
+    
+    
+    def __init__(self):
         
+        self.dataframe=self.get_data_frame(path)
+               
+        self.media={}        
+        self.variancia={}
+        self.desvio={}
+    
+        self.soma={}               
+    
--- nic-organizado-pylint/PatternsParser.py
+++ nic-organizado-pylint/PatternsParser.py
@@ -89,14 +89,14 @@ class FromImgToCSV():
     '''Classe que pega o array dado, transforma em imagem do tipo OpenCV e conta padrões
     '''
     def __init__(self, start_value, patterns, game_of_life):
-        self.alive_pixel = start_value
+        self.alive_ptxel = start_value
         self.dead_pixel = start_value
         #1)começar um dicionario 
         self.pattern_array_dict = self.start_pattern_array_dict(patterns, start_value)
         self.game_of_life_video = self.start_game_of_life_video(game_of_life)
 
         self.game_of_life_array = game_of_life
-        self.pattern_image_dict = self.start_pattern_image_dict(self.pattern_array_dict)
+        self.pattern_image_dict = self.start_pattern_image_version_dict(self.pattern_array_dict)
         index_list = [i for i in range(0, len(self.game_of_life_array))]
 
         empty_array = np.zeros(len(self.game_of_life_array))
@@ -145,17 +145,15 @@ class FromImgToCSV():
         game_of_life_video = []
         i=0
         for frame in game_of_life_array:
-            frame_array = frame.astype('uint8')*255
-            #print(frame)
-            #print(frame_array)
+            frame_array = frame.copy().astype('uint8')*255
             #printar essas imgs geradsa
             ret,frame_img = cv2.threshold(frame_array, 0, 255, cv2.THRESH_BINARY)
-            cv2.imwrite('data/imgtest/'+ str(i) + '.png', frame_img)
+            #cv2.imwrite('data/imgtest/'+ str(i) + '.png', frame_img)
             game_of_life_video.append(frame_img)
-            i+=1
+            #i+=1
         return game_of_life_video
 
-    def start_pattern_image_dict(self, pattern_array_dict):
+    def start_pattern_image_version_dict(self, pattern_array_dict):
         pattern_image_dict = pattern_array_dict
         for name in pattern_image_dict:
             for k in range(len(pattern_image_dict[name])):
@@ -166,40 +164,112 @@ class FromImgToCSV():
         pattern_image_array = pattern_array_dict[pattern_name][k].astype('uint8')*255
         #printar essas imgs geradas
         ret, img = cv2.threshold(pattern_image_array, 0, 255, cv2.THRESH_BINARY)
-        cv2.imwrite('data/imgtest/'+ pattern_name + str(k) + '.png',img)
+        #cv2.imwrite('data/imgtest/'+ pattern_name + str(k) + '.png',img)
         return img
 
     def find_patterns(self):
-        for frame in range(0, len(self.game_of_life_video)):
+        for frame_number in range(0, len(self.game_of_life_video)):
             for pattern in self.pattern_image_dict:
-                count_pattern = 0
+                amnt_matches = 0
+                #print(pattern)
+                #k=0
                 for pattern_version in self.pattern_image_dict[pattern]:
                     
-                    #print(pattern, len(result[1]))
-                self.pattern_df[pattern][frame] += len(result[1])
+                    amnt_matches = self.crop_borders(pattern_version, self.game_of_life_video[frame_number])
+                    #k+=1
+                    #print(amnt_matches)
+                    self.pattern_df[pattern][frame_number] += amnt_matches
         print(self.pattern_df)
         self.pattern_df.to_csv('data/files/csv/teste.csv', encoding = 'utf8')
 
-    def get_amnt_matches(self, frame, pattern):
-        result = cv2.matchTemplate(self.game_of_life_video[frame], pattern_version, cv2.TM_CCOEFF_NORMED)
-        threshold = 1
-        return len(np.where(result >= threshold))
-                    
-    def crop_borders(self, pattern_image, frame):
-        amnt_matches = 0
-        pi_height,pi_width = pattern_image.shape
+    def get_amnt_matches(self, frame, pattern_image_version):
+        #print(frame)
+        pt_height,pt_width = pattern_image_version.shape
         f_height, f_width = frame.shape
-        #1)
-        pattern_image_crop_1 = pattern_image[1:pi_height, 1:pi_width]
-        frame_1 = frame[0:pi_height, 0:pi_width]
-        amnt_matches += self.get_amnt_matches(frame_1, pattern_image_crop_1) 
-        #2)
-        pattern_image_crop_2 = pattern_image[1:pi_height, 0:pi_width]
-        frame_2 = frame[0:pi_height-1, pi_width:f_width-pi_width]
-        amnt_matches += self.get_amnt_matches(frame_2, pattern_image_crop_2) 
+        #print(pt_height, pt_width, f_height, f_width)
+        if 0 not in (pt_height, f_height, pt_width, f_width):
+            if (f_height > pt_height) and (f_width > pt_width):
+                result = cv2.matchTemplate(frame, pattern_image_version, cv2.TM_CCOEFF_NORMED)
+                threshold= .778
+                count = len(np.where(result >= threshold)[0]) + len(np.where(result <= -threshold)[0])
+                return count
+            else:
+                return 0
+        else:
+            #print('tem 0')
+            return 0
+        return 0
+        #print('afdsklça')
+                    
+    def crop_borders(self, pattern_image_version, frame):
+        amnt_matches = 0
+        pt_height,pt_width = pattern_image_version.shape
+        f_height, f_width = frame.shape
+        #print(pt_height, pt_width, f_height, f_width)
+        #no meio:
+        #cv2.imwrite('data/imgtest/' + str(frame_number) +'.png',frame)
+        amnt_matches += self.get_amnt_matches(frame, pattern_image_version)
         
-        #6)
-        pattern_image_crop_2 = pattern_image[1:pi_height, 0:pi_width]
-        frame_2 = frame[0:pi_height-1, pi_width:f_width-pi_width]
-        amnt_matches += self.get_amnt_matches(frame_2, pattern_image_crop_2) 
+        
+        #Área 1)
+        pattern_image_version_crop_1 = pattern_image_version[1:, 1:]
+        frame_1 = frame[:pt_height, :pt_width]
+        #print(pt_height, frame_1.shape[0])
+        #cv2.imwrite('data/imgtest/' + str(frame_number) +'_1.png',frame_1)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) + '_1.png',pattern_image_version_crop_1)
+        #print(frame_1, '\n', pattern_image_version_crop_1)
+        amnt_matches += self.get_amnt_matches(frame_1, pattern_image_version_crop_1) 
+        
+        #Área 2)
+        pattern_image_version_crop_2 = pattern_image_version[1:, :]
+        frame_2 = frame[:pt_height, pt_width:f_width-pt_width]
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_2.png',frame_2)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_2.png',pattern_image_version_crop_2)
+        
+        amnt_matches += self.get_amnt_matches(frame_2, pattern_image_version_crop_2) 
+        
+        #Área 3)
+        pattern_image_version_crop_3 = pattern_image_version[1:, :pt_width-1]
+        frame_3 = frame[:pt_height, f_width - pt_width:]
+        amnt_matches += self.get_amnt_matches(frame_3, pattern_image_version_crop_3) 
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_3.png',frame_3)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_3.png',pattern_image_version_crop_3)
+        
+        #Área 4)
+        pattern_image_version_crop_4 = pattern_image_version[:, :pt_width-1]
+        frame_4 = frame[pt_height:f_height-pt_height, f_width-pt_width:]
+        amnt_matches += self.get_amnt_matches(frame_4, pattern_image_version_crop_4) 
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_4.png',frame_4)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_4.png',pattern_image_version_crop_4)
+        
+        
+        #Área 5)
+        pattern_image_version_crop_5 = pattern_image_version[:pt_height-1, :pt_width-1]
+        frame_5 = frame[f_height-pt_height:, f_width-pt_width:]
+        amnt_matches += self.get_amnt_matches(frame_5, pattern_image_version_crop_5) 
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_5.png',frame_5)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_5.png',pattern_image_version_crop_5)
+        
+        #Área 6)
+        pattern_image_version_crop_6 = pattern_image_version[:pt_height-1, :]
+        frame_6 = frame[f_height-pt_height:f_height, pt_width:f_width-pt_width]
+        amnt_matches += self.get_amnt_matches(frame_6, pattern_image_version_crop_6) 
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_6.png',frame_2)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_6.png',pattern_image_version_crop_6)
+        
+        #Área 7)
+        pattern_image_version_crop_7 = pattern_image_version[:pt_height-1, 1:]
+        frame_7 = frame[f_height-pt_height:, :pt_width]
+        amnt_matches += self.get_amnt_matches(frame_7, pattern_image_version_crop_7) 
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_7.png',frame_7)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_7.png',pattern_image_version_crop_7)
+        
+        #Área 8)
+        pattern_image_version_crop_8 = pattern_image_version[:, 1:]
+        frame_8 = frame[pt_height:f_height - pt_height, :pt_width]
+        amnt_matches += self.get_amnt_matches(frame_8, pattern_image_version_crop_8)
+        #cv2.imwrite('data/imgtest/' + str(frame_number)  + '_8.png',frame_8)
+        #cv2.imwrite('data/imgtest/' + pattern_name + str(k) +'_8.png',pattern_image_version_crop_8)
+        
+        return amnt_matches 
         
